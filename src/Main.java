d3ceb23c4f07213a9f460f636e781f94bb646e4d
LandonTopy <landontopy@gmail.com>
2023-09-15 09:18:24 -0400

    p

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Variables
+        int ccBalance = 5000;
+        double interest = 1.17;
+        double month1 = ccBalance * interest;
+        double month2 = month1 * interest;
+
+        System.out.println("The total balance after interest after 1 month is $" + month1);
+        System.out.println("The total balance after interest after 2 months is $" + month2);
     }
 }
